62446d7715f9d7cb1f398488dee0ea1c641c5938
Troy Scott <troys2005@gmail.com>
2017-03-06 23:07:54 -0800

    PropTypes and DefaultProps using a stateless functional component

--- src/Summary.js
+++ src/Summary.js
@@ -1,11 +1,47 @@
 import React, { PropTypes } from 'react'
 
+// Stateless Functional Component
+// Similar to the es6 classes
+
+const Summary = ({ ingredients, steps, title}) => {
+    return <div className="summary">
+            <h1>{title}</h1>
+            <p>{ingredients} Ingredients|{steps} Steps</p>
+    </div>
+}
+
+Summary.propTypes = {
+    ingredients: PropTypes.number.isRequired,
+    steps: PropTypes.number.isRequired
+}
+
+Summary.defaultProps = {
+    ingredients: 1,
+    steps: 1,
+    title: '[recipes]'
+
+}
+
+
+export default Summary
+
+
+
+/*
+
+// React.createClass approach
+
 const Summary = React.createClass({
     displayName: "Summary",
     propTypes: {
         ingredients: PropTypes.number.isRequired,
         steps: PropTypes.number.isRequired,
-        title: PropTypes.string
+        title: (props, propName) =>
+            (typeof props[propName] !== 'string') ?
+                new Error("A title must be a string") :
+                (props[propName].length > 20) ?
+                new Error('title is over 20 characters'):
+                null
     },
     getDefaultProps() {
         return {
@@ -28,5 +64,4 @@ const Summary = React.createClass({
     }
 })
 
-
-export default Summary
+*/
